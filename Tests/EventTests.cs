2a15eda7ce9f004dff42fc911962db7f01fcd48b
Ivo Mäeoja <maeoja@outlook.com>
2022-06-29 21:06:55 +0300

    test: Partipation_Count_For_Company_And_Person

--- Tests/EventTests.cs
+++ Tests/EventTests.cs
@@ -157,11 +157,75 @@ public class EventTests
         _bll.Participations.Add(participation);
         await _bll.SaveChangesAsync();
         
-        var result = eventMapper.Map(await _bll.GetEventWithParticipantsCount(eventId));       
+        var result = eventMapper.Map(await _bll.GetEventWithParticipantsCount(eventId));
 
         Assert.NotNull(result);
         Assert.IsType<DTO.Public.Event>(result);
         Assert.Equal(12, result.TotalParticipants);
     }
 
+    [Fact]
+    public async void Test_Event_Returns_Correct_Partipation_Count_For_Company_People_And_Person()
+    {
+        var eventMapper = new EventMapper(_mapper);
+
+        var @event = new DTO.ServiceEntity.Event
+        {
+            Name = "Test event",
+            StartTime = DateTime.UtcNow,
+            Location = "Testland",
+            Notes = "Test notes"
+        };
+
+        var company = new DTO.ServiceEntity.Company
+        {
+            Name = "Test company",
+            Code = 77000312,
+            ParticipantsCount = 2,
+            Notes = "Accountants department"
+        };
+
+        var paymentType = new DTO.ServiceEntity.PaymentType
+        {
+            Type = "Sularaha"
+        };
+
+        var person = new DTO.ServiceEntity.Person
+        {
+            FirstName = "TesFirstName",
+            LastName = "TestLastName",
+            PersonalIdentificationCode = "34501234215",
+            Notes = "TestNotes"
+        };
+
+        var eventId = _bll.Events.Add(@event).Id;
+        var companyId = _bll.Companies.Add(company).Id;
+        var personId = _bll.Persons.Add(person).Id;
+        var paymentTypeId = _bll.PaymentTypes.Add(paymentType).Id;
+
+        var personParticipation = new DTO.ServiceEntity.Participation
+        {
+            EventId = eventId,
+            PersonId = personId,
+            PaymentTypeId = paymentTypeId
+        };
+
+        var companyParticipation = new DTO.ServiceEntity.Participation
+        {
+            EventId = eventId,
+            CompanyId = companyId,
+            PaymentTypeId = paymentTypeId
+        };
+
+        _bll.Participations.Add(personParticipation);
+        _bll.Participations.Add(companyParticipation);
+
+        await _bll.SaveChangesAsync();
+
+        var result = eventMapper.Map(await _bll.GetEventWithParticipantsCount(eventId));
+
+        Assert.NotNull(result);
+        Assert.IsType<DTO.Public.Event>(result);
+        Assert.Equal(3, result.TotalParticipants);
+    }
 }
