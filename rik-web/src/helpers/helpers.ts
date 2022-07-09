8e53dd8668ec606e68d3435b64d55e7ae3394e4e
Ivo Mäeoja <maeoja@outlook.com>
2022-07-09 19:50:41 +0300

    feat: ParticipationEvent deleteItem & TG to helper

--- rik-web/src/helpers/helpers.ts
+++ rik-web/src/helpers/helpers.ts
@@ -1,7 +1,18 @@
-export default function fixStartTimeFormat(startTime: string) {
+import type ICompany from "@/domain/ICompany";
+import type IPerson from "@/domain/IPerson";
+
+export function fixStartTimeFormat(startTime: string) {
     const [date, time] = startTime.split("T");
     const [year, month, day] = date.split("-");
     const [hours, minutes] = time.split(":");
 
     return `${day}.${month}.${year} ${hours}:${minutes}`;
+}
+
+export function isIPerson(person: any): person is IPerson {
+    return (person as IPerson).fullName !== undefined;
+}
+
+export function isICompany(company: any): company is ICompany {
+    return (company as ICompany).participantsCount !== undefined;
 }
--- rik-web/src/views/participation/ParticipationEvent.vue
+++ rik-web/src/views/participation/ParticipationEvent.vue
@@ -1,22 +1,79 @@
 <script setup lang="ts">
 import { useEventStore } from '@/stores/eventStore';
 import { useParticipationStore } from '@/stores/participationStore';
-import fixStartTimeFormat from '@/helpers/helpers';
+import { fixStartTimeFormat, isICompany, isIPerson } from '@/helpers/helpers';
 import router from '@/router/router';
+import { usePersonStore } from '@/stores/personStore';
+import { useCompanyStore } from '@/stores/companyStore';
+import type IPerson from '@/domain/IPerson';
+import type ICompany from '@/domain/ICompany';
+import type IParticipation from '@/domain/IParticipation';
+import { ref, type Ref } from 'vue';
 
 const participationStore = useParticipationStore();
 await participationStore.fillParticipations();
 
 const eventStore = useEventStore();
-
 const event = eventStore.$state.events.find((e) => e.id === props.id);
 
+const personStore = usePersonStore();
+await personStore.fillPersons();
+
+const companyStore = useCompanyStore();
+await companyStore.fillCompanies();
 
 const props = defineProps({
     id: String
 });
 
+const persons: Ref<IPerson[]> = ref([]);
+const companies: Ref<ICompany[]> = ref([]);
+const participations: IParticipation[] = participationStore.$state.participations.filter((p) => p.eventId === props.id);
 
+participations.forEach(element => {
+    if (element.personId !== null) {
+        const person = personStore.$state.persons.find((p) => p.id === element.personId);
+        if (isIPerson(person)) {
+            persons.value.push(person);
+        }
+    } else if (element.companyId !== null) {
+        const company = companyStore.$state.companies.find((c) => c.id === element.companyId);
+        if (isICompany(company)) {
+            companies.value.push(company);
+        }
+    }
+});
+
+async function deleteItem(item: IPerson | ICompany) {
+    let participationId: string | undefined;
+
+    if (isIPerson(item)) {
+        participationId = participations.find((p) => p.personId === item.id && p.eventId === event?.id)?.id;
+
+        if (participationId !== undefined) {
+            await participationStore.deleteParticipation(participationId);
+        }
+
+        persons.value = persons.value.filter((p) => p.id !== item.id);
+
+        if (item.id !== undefined) {
+            await personStore.deletePerson(item.id);
+        }
+
+    } else if (isICompany(item)) {
+        participationId = participations.find((p) => p.companyId === item.id && p.eventId === event?.id)?.id;
+
+        if (participationId !== undefined) {
+            await participationStore.deleteParticipation(participationId);
+        }
+
+        companies.value = companies.value.filter((c) => c.id !== item.id);
+
+        if (item.id !== undefined) {
+            await companyStore.deleteCompany(item.id);
+        }
+    }
+}
 
 </script>
 
@@ -40,5 +97,24 @@ const props = defineProps({
             </li>
         </ul>
 
+        <table class="table table-sm">
+            <thead>
+                <th class="colspan-3">
+                    Osavõtjad
+                </th>
+            </thead>
+            <tbody>
+                <tr v-for="person in persons" v-bind:key="person.id">
+                    <td>{{ person.fullName }}</td>
+                    <td>{{ person.personalIdentificationCode }}</td>
+                    <td><img type="button" v-on:click="deleteItem(person)" src="../../img/remove.svg" height="20"></td>
+                </tr>
+                <tr v-for="company in companies" v-bind:key="company.id">
+                    <td>{{ company.name }}</td>
+                    <td>{{ company.code }}</td>
+                    <td><img type="button" v-on:click="deleteItem(company)" src="../../img/remove.svg" height="20"></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
