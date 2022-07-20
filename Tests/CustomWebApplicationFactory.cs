c25b3d9be44491f69501cf52bd9c130783e29892
Ivo Mäeoja <maeoja@outlook.com>
2022-07-20 15:49:26 +0300

    tests: CustomWebAppFactory w TStartup to switch DB

--- Tests/CustomWebApplicationFactory.cs
+++ Tests/CustomWebApplicationFactory.cs
@@ -1,6 +1,32 @@
 namespace Tests;
+
+using Domain;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup> where TStartup : class {
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services => {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
 
+            if (descriptor != null) {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+            
+            db.Database.EnsureCreated();
+        });
+    }
 }
